727d804922920f79422399c0c6fe30573dec3c2f
Nhi Vu <vuthuynhi05@gmail.com>
2024-02-21 15:10:05 -0600

    added saved address custom options

--- Frontend/Frontend/AccountPage/SavedAddressView.swift
+++ Frontend/Frontend/AccountPage/SavedAddressView.swift
@@ -8,8 +8,63 @@
 import SwiftUI
 
 struct SavedAddressView: View {
+    // Example data - replace with your actual data source
+        let savedAddresses = [
+            ("Home", "1234 Street Dr"),
+            ("Work", "1234 Street Dr"),
+            ("Home", "1234 Street Dr"),
+            ("Home", "1234 Street Dr"),
+            ("Home", "1234 Street Dr"),
+            ("Home", "1234 Street Dr"),
+            ("Home", "1234 Street Dr"),
+            ("Home", "1234 Street Dr"),
+            ("Home", "1234 Street Dr"),
+            ("Home", "1234 Street Dr"),
+            ("Work", "1234 Street Dr"),
+            ("Home", "1234 Street Dr"),
+            ("Home", "1234 Street Dr"),
+            ("Home", "1234 Street Dr"),
+            ("Home", "1234 Street Dr"),
+            ("Home", "1234 Street Dr"),
+            ("Home", "1234 Street Dr"),
+            ("Home", "1234 Street Dr"),
+            
+            // Add more addresses as needed
+        ]
+    
     var body: some View {
-        Text("Saved Addresses!")
+        NavigationView {
+            List {
+                // "Add Saved Place" button
+                NavigationLink(destination: AddSavedAddress()) {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Add Saved Place")
+                                .foregroundColor(.blue)
+                            Text("Get to your favorite destination faster")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                ForEach(savedAddresses, id: \.self.0) { label, address in
+                    HStack{
+                        Image(systemName: "star.circle.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                        VStack(alignment: .leading) {
+                            Text(label)
+                                .foregroundColor(.black)
+                            Text(address)
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Choose a Place")
+        }
     }
 }
 
--- Frontend/Frontend/Pages/AccountPageView.swift
+++ Frontend/Frontend/Pages/AccountPageView.swift
@@ -77,6 +77,7 @@ struct AccountPageView: View {
             .onAppear {
                 loadGoogleUserInfo()
             }
+            .navigationBarBackButtonHidden(true)
         }
     }
     
