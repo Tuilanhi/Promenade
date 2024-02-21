e6460c28899daaee80e901f51bdffa99c28a82c2
Nhi Vu <vuthuynhi05@gmail.com>
2024-02-21 11:34:08 -0600

    added activity page mock up

---
+++ Frontend/Frontend/Pages/ActivityPageView.swift
@@ -0,0 +1,54 @@
+//
+//  ActivityPageView.swift
+//  Frontend
+//
+//  Created by nhi vu on 2/21/24.
+//
+
+import SwiftUI
+
+struct ActivityPageView: View {
+    // Mock activity data
+    var activities = [
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        Activity(finalDestination: "366 East Golden Star Dr.", date: "Feb 14 · 9:30 PM", price: 25.16),
+        
+    ]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 16) {
+                    Text("Past")
+                        .font(.headline)
+                        .padding(.leading)
+                        .padding(.top)
+                    
+                    ForEach(activities) { activity in
+                        ActivityCard(activity: activity)
+                    }
+                }
+            }
+            .navigationTitle("Activity")
+        }
+    }
+}
+
+#Preview {
+    ActivityPageView()
+}
