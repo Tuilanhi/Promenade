a226126a5074e43da7276785dcd1e63aa92f5a4d
James Stautler <jstautler10@gmail.com>
2024-02-25 18:26:01 -0600

    destination-selection skeleton

--- Frontend/Frontend/Pages/DestinationSelectionPageView.swift
+++ Frontend/Frontend/Pages/DestinationSelectionPageView.swift
@@ -6,34 +6,55 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct DestinationSelectionPageView: View {
-    
-    @State private var start: String = ""
-    @State private var destination: String = ""
-    
-    
+    @State private var startLocation = ""
+    @State private var destinationLocation = ""
     var body: some View {
-        
         VStack {
-            TextField(
-                "Enter Start",
-                text: $start
-            )
-            .border(Color.black)
-            .multilineTextAlignment(.center)
-            .padding([.leading, .trailing], 25)
+            // Header (text field) view
+            HStack {
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 6, height: 6)
+                    
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 24)
+                    
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: 6, height: 6)
+                }
+                VStack {
+                    TextField("Current Location", text:$startLocation)
+                        .frame(height: 32)
+                        .background(Color(.systemGroupedBackground))
+                        .padding(.trailing)
+                    
+                    TextField("Destination", text:$destinationLocation)
+                        .frame(height: 32)
+                        .background(Color(.systemGray4))
+                        .padding(.trailing)
+
+                }
+            }
+            .padding(.horizontal)
+            .padding(.top, 32)
             
-            TextField(
-                "Enter Destination",
-                text: $destination
-            )
-            .border(Color.black)
-            .multilineTextAlignment(.center)
-            .padding([.leading, .trailing], 25)
+            Divider()
+                .padding(.vertical)
+    
+            ScrollView {
+                VStack(alignment: .leading) {
+                    ForEach(0 ..< 20, id: \.self) { _ in
+                        DestinationSelectionResultCell()
+                    }
+                }
+            }
         }
-        .padding([.top, .bottom], 50)
-        Spacer()
     }
 }
 
