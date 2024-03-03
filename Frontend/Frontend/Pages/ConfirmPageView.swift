e2d7431ce36e42726a5671acd085f64656e4f47e
Nicole Clements <112514757+mnclements@users.noreply.github.com>
2024-03-02 19:34:11 -0600

    route added to confirm page

--- Frontend/Frontend/Pages/ConfirmPageView.swift
+++ Frontend/Frontend/Pages/ConfirmPageView.swift
@@ -9,35 +9,105 @@ import SwiftUI
 import MapKit
 
 struct ConfirmPageView: View {
-    // State variable to hold the map region
+    @State private var cameraPosition: MapCameraPosition = .region(.userRegion)
     @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 30.613, longitude: -96.342), // Example coordinates
+        center: CLLocationCoordinate2D(latitude: 30.613, longitude: -96.342),
         span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
     )
+    @State private var getDirections = true
+    @State private var routeDisplaying = true
+    @State private var route: MKRoute?
+    @State private var routeDestination: MKMapItem?
     
     @State private var address: String = "161 Wellborn Rd"
     
     var body: some View {
-
-            
-        VStack {
-            MapView(region: $region)
-                .frame(height: UIScreen.main.bounds.height * 0.70)
-                .edgesIgnoringSafeArea(.top)
-
-            Text("Confirm Pickup Spot")
-                .font(.title)
-                .fontWeight(.bold)
-                .foregroundColor(.black)
-                .padding(.top, 0)
-                
-         
-            Divider()
         
+        VStack {
+            
+            Map(position: $cameraPosition) {
+                Annotation("Your Location", coordinate: .userLocation) {
+                    ZStack {
+                        Circle()
+                            .frame(width: 32, height: 32)
+                            .foregroundColor(.blue.opacity(0.25))
+                        Circle()
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(.white)
+                        Circle()
+                            .frame(width: 12, height: 12)
+                            .foregroundColor(.blue)
+                    }
+                }
+                
+                Marker("Pick up Point", coordinate: .userDestination)
+                
+                
+                if let route {
+                    MapPolyline(route.polyline)
+                        .stroke(.blue, lineWidth: 6)
+                }
+                
+            }
+            
+            Capsule()
+                .foregroundColor(Color(.systemGray5))
+                .frame(width: 48, height: 6)
+            
             HStack {
-                Spacer()
-                Text("Address: \(address)")
-                Spacer()
+                
+                VStack{
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 8, height: 8)
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width:1, height:32)
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: 8, height:8)
+                    
+                }
+                // walk info
+                VStack(alignment: .leading, spacing: 24) {
+                    HStack {
+                        Text("Your location")
+                            .font(.system(size: 16, weight: .semibold))
+                            .foregroundColor(.gray)
+                        Spacer()
+                        Text("1:30 PM")
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(.gray)
+                    }
+                    .padding(.bottom, 10)
+                    
+                    HStack {
+                        Text("Zachry")
+                            .font(.system(size: 16, weight: .semibold))
+                        Spacer()
+                        Text("1:45 PM")
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(.gray)
+                    }
+                }
+                .padding(.leading, 8)
+            }
+            .padding()
+            
+            
+            Divider()
+                .padding(.vertical, 8)
+            
+            HStack {
+                Button {
+                } label : {
+                    Text("CONFIRM PICK UP POINT")
+                        .fontWeight(.bold)
+                        .frame(width: UIScreen.main.bounds.width-94, height: 50)
+                        .background(.blue)
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                }
                 Button(action: {}) {
                     Image(systemName: "magnifyingglass")
                         .padding()
@@ -45,21 +115,45 @@ struct ConfirmPageView: View {
                         .background(Color.black)
                         .cornerRadius(10)
                 }
-                Spacer()
             }
+        }
+        .background(.white)
+        .onAppear {
+            fetchRoute()
+        }
+    }
+}
+
+extension CLLocationCoordinate2D {
+    static var userLocation: CLLocationCoordinate2D {
+        return .init(latitude: 30.613, longitude: -96.342)
+    }
+}
+
+extension CLLocationCoordinate2D {
+    static var userDestination: CLLocationCoordinate2D {
+        return .init(latitude: 30.619049, longitude: -96.339394)
+    }
+}
+
+extension MKCoordinateRegion {
+    static var userRegion: MKCoordinateRegion {
+        return .init(center: .userLocation, latitudinalMeters: 1000, longitudinalMeters: 1000)
+    }
+}
+
+extension ConfirmPageView {
+    
+    func fetchRoute() {
         
-            Spacer()
-            
-            Button(action: {}) {
-                Text("Confirm")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding()
-                    .foregroundColor(Color.white)
-                    .background(Color.black)
-                    .cornerRadius(10)
-            }
-      
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: .init(coordinate: .userLocation))
+        request.destination = MKMapItem(placemark: .init(coordinate: .userDestination))
+        
+        Task {
+            let result = try? await MKDirections(request: request).calculate()
+            route = result?.routes.first
+            routeDestination =  MKMapItem(placemark: .init(coordinate: .userDestination))
         }
     }
 }
