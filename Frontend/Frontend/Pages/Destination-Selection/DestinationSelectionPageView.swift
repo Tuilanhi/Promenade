0f5e5e811f97e7f19f98ed5f0d01ea341fdc40eb
Nicole Clements <112514757+mnclements@users.noreply.github.com>
2024-03-03 14:19:47 -0600

    Revert "update destination selection with main"

---
+++ Frontend/Frontend/Pages/Destination-Selection/DestinationSelectionPageView.swift
@@ -0,0 +1,65 @@
+//
+//  DestinationSelectionView.swift
+//  Frontend
+//
+//  Created by nhi vu on 2/20/24.
+//
+
+import SwiftUI
+import UIKit
+
+struct DestinationSelectionPageView: View {
+    @State private var startLocation = ""
+    @State private var destinationLocation = ""
+    var body: some View {
+        VStack {
+            // Header (text field) view
+            HStack {
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 6, height: 6)
+                    
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 24)
+                    
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: 6, height: 6)
+                }
+                VStack {
+                    TextField("Current Location", text:$startLocation)
+                        .frame(height: 32)
+                        .background(Color(.systemGroupedBackground))
+                        .padding(.trailing)
+                        .padding(.leading)
+                    
+                    TextField("Destination", text:$destinationLocation)
+                        .frame(height: 32)
+                        .background(Color(.systemGray4))
+                        .padding(.trailing)
+                        .padding(.leading)
+
+                }
+            }
+            .padding(.horizontal)
+            .padding(.top)
+            
+            Divider()
+                .padding(.vertical)
+    
+            ScrollView {
+                VStack(alignment: .leading) {
+                    ForEach(0 ..< 20, id: \.self) { _ in
+                        DestinationSelectionResultCell()
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    DestinationSelectionPageView()
+}
---
+++ Frontend/Frontend/Pages/Ride-Selection/RideSelectionView.swift
@@ -0,0 +1,120 @@
+//
+//  RideSelectionView.swift
+//  Frontend
+//
+//  Created by James Stautler on 2/27/24.
+//
+
+import SwiftUI
+
+struct RideSelectionView: View {
+    var body: some View {
+        VStack {
+            Capsule()
+                .foregroundColor(Color(.systemGray5))
+                .frame(width: 48, height: 6)
+            // Trip Information
+            HStack {
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 8, height: 8)
+                    
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 32)
+                    
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: 8, height: 8)
+                }
+                
+                VStack(alignment: .leading, spacing: 24) {
+                    HStack {
+                        Text("Current Location")
+                            .font(.system(size: 16, weight: .semibold))
+                            .foregroundColor(.gray)
+                        
+                        Spacer()
+                        
+                        Text("1:30 PM")
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(.gray)
+                    }
+                    .padding(.bottom, 10)
+                    
+                    HStack {
+                        Text("Destination")
+                            .font(.system(size: 16, weight: .semibold))
+                        
+                        Spacer()
+                        
+                        Text("1:45 PM")
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(.gray)
+                    }
+                }
+                .padding(.leading, 8)
+            }
+            .padding()
+            
+            Divider()
+            
+            // Route Selection
+            Text("Suggested Routes")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .padding()
+                .foregroundColor(Color.gray)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 12) {
+                    ForEach(0 ..< 3, id: \.self) { _ in
+                        VStack(alignment: .leading) {
+                            Image("turtle")
+                                .resizable()
+                                .scaledToFit()
+                            
+                            VStack(spacing: 4) {
+                                Text("Walk-Ride")
+                                    .font(.system(size: 14, weight: .semibold))
+                                
+                                Text("0.6 miles")
+                                    .font(.system(size: 14, weight: .semibold))
+                                
+                                Text("$10.35")
+                                    .font(.system(size: 14, weight: .semibold))
+                            }
+                            .padding(8)
+                            
+                        }
+                        .frame(width:112, height: 140)
+                        .background(Color(.systemGroupedBackground))
+                        .cornerRadius(10)
+                    }
+                }
+            }
+            .padding(.horizontal)
+            
+            Divider()
+                .padding(.vertical, 8)
+            
+            Button {
+                
+            } label: {
+                Text("CONFIRM ROUTE")
+                    .fontWeight(.bold)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 50)
+                    .background(.blue)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+            }
+        }
+        .background(.white)
+    }
+}
+
+#Preview {
+    RideSelectionView()
+}
