6ef1c12fffaaccb5037724248a118eda439683e2
James Stautler <jstautler10@gmail.com>
2024-03-03 23:03:01 -0600

    search completion

---
+++ Frontend/Frontend/Destination-Selection/LocationSearchViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  LocationSearchViewModel.swift
+//  Frontend
+//
+//  Created by James Stautler on 3/3/24.
+//
+
+import Foundation
+import MapKit
+
+class LocationSearchViewModel: NSObject, ObservableObject {
+    // Properties
+    @Published var results = [MKLocalSearchCompletion]()
+    private let searchCompleter = MKLocalSearchCompleter()
+    var queryFragment: String = "" {
+        didSet {
+            searchCompleter.queryFragment = queryFragment
+        }
+    }
+    
+    override init() {
+        super.init()
+        searchCompleter.delegate = self
+        searchCompleter.queryFragment = queryFragment
+    }
+}
+
+extension LocationSearchViewModel: MKLocalSearchCompleterDelegate {
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        self.results = completer.results
+    }
+}
