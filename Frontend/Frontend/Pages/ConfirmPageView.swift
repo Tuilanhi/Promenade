2247c3f4f4c9b26ba0637b4aa6bc3a2e4b3c5a48
Nicole Clements <112514757+mnclements@users.noreply.github.com>
2024-03-26 23:14:30 -0500

    updated confirm page map with locations from ride selection

--- Frontend/Frontend/Pages/ConfirmPageView.swift
+++ Frontend/Frontend/Pages/ConfirmPageView.swift
@@ -9,20 +9,41 @@ import SwiftUI
 import MapKit
 
 struct ConfirmPageView: View {
-    @State private var cameraPosition: MapCameraPosition = .region(.userRegion)
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 30.613, longitude: -96.342),
-        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-    )
+//    @State private var cameraPosition: MapCameraPosition = .region(.userRegion)
+//    @State private var region = MKCoordinateRegion(
+//        center: CLLocationCoordinate2D(latitude: 30.613, longitude: -96.342),
+//        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+//    )
     @State private var getDirections = true
     @State private var routeDisplaying = true
     @State private var route: MKRoute?
     @State private var routeDestination: MKMapItem?
     
     @State private var address: String = "161 Wellborn Rd"
+<<<<<<< Updated upstream
+=======
+ 
+    @State private var showOrderPage = false
+    @StateObject var manager = LocationManager()
+    
+    let currentTime: String
+    let destinationTime: String
+    @State private var userCurrentLocation: CLLocationCoordinate2D
+//    let userCurrentLocation: CLLocationCoordinate2D
+    let userPickup: CLLocationCoordinate2D
+
+    init(currentTime: String, destinationTime: String, userCurrentLocation: CLLocationCoordinate2D, userPickup: CLLocationCoordinate2D) {
+        self.currentTime = currentTime
+        self.destinationTime = destinationTime
+        self.userCurrentLocation = userCurrentLocation
+        self.userPickup = userPickup
+    }
+>>>>>>> Stashed changes
     
     var body: some View {
+
         
+<<<<<<< Updated upstream
         VStack {
             
             Map(position: $cameraPosition) {
@@ -38,6 +59,108 @@ struct ConfirmPageView: View {
                             .frame(width: 12, height: 12)
                             .foregroundColor(.blue)
                     }
+=======
+        if showOrderPage {
+            OrderPageView()
+        } else {
+            NavigationView{
+                VStack {
+                    Map/*(position: $cameraPosition)*/ {
+                        Annotation("Your Location", coordinate: userCurrentLocation) {
+                            ZStack {
+                                Circle()
+                                    .frame(width: 32, height: 32)
+                                    .foregroundColor(.blue.opacity(0.25))
+                                Circle()
+                                    .frame(width: 20, height: 20)
+                                    .foregroundColor(.white)
+                                Circle()
+                                    .frame(width: 12, height: 12)
+                                    .foregroundColor(.blue)
+                            }
+                        }
+                        
+                        // need to get coordinate from ride selection
+                        Marker("Pickup Point", coordinate: userPickup)
+                    
+                        if let route {
+                            MapPolyline(route.polyline)
+                                .stroke(.blue, lineWidth: 6)
+                        }
+                        
+                    }
+                    
+                    Capsule()
+                        .foregroundColor(Color(.systemGray5))
+                        .frame(width: 48, height: 6)
+                    
+                    HStack {
+                        
+                        VStack{
+                            Circle()
+                                .fill(Color(.systemGray3))
+                                .frame(width: 8, height: 8)
+                            Rectangle()
+                                .fill(Color(.systemGray3))
+                                .frame(width:1, height:32)
+                            Rectangle()
+                                .fill(.black)
+                                .frame(width: 8, height:8)
+                            
+                        }
+                        // walk info
+                        VStack(alignment: .leading, spacing: 24) {
+                            HStack {
+                                Text("Your location")
+                                    .font(.system(size: 16, weight: .semibold))
+                                    .foregroundColor(.gray)
+                                Spacer()
+                                Text(currentTime)
+                                    .font(.system(size: 14, weight: .semibold))
+                                    .foregroundColor(.gray)
+                            }
+                            .padding(.bottom, 10)
+                            
+                            HStack {
+                                Text("Pick up Point")
+                                    .font(.system(size: 16, weight: .semibold))
+                                Spacer()
+                                Text(destinationTime)
+                                    .font(.system(size: 14, weight: .semibold))
+                                    .foregroundColor(.gray)
+                            }
+                        }
+                        .padding(.leading, 8)
+                    }
+                    .padding()
+                    
+                    
+                    Divider()
+                        .padding(.vertical, 8)
+                    
+                    HStack {
+                        
+                        Button(action: {
+                            showOrderPage=true
+                        }) {
+                            Text("CONFIRM PICK UP POINT")
+                                .fontWeight(.bold)
+                                .frame(width: UIScreen.main.bounds.width-94, height: 50)
+                                .background(.blue)
+                                .cornerRadius(10)
+                                .foregroundColor(.white)
+                        }
+                        
+                        Button(action: {}) {
+                            Image(systemName: "magnifyingglass")
+                                .padding()
+                                .foregroundColor(Color.white)
+                                .background(Color.black)
+                                .cornerRadius(10)
+                        }
+                    }
+                    .padding(.vertical)
+>>>>>>> Stashed changes
                 }
                 
                 Marker("Pick up Point", coordinate: .userDestination)
@@ -124,40 +247,43 @@ struct ConfirmPageView: View {
     }
 }
 
-extension CLLocationCoordinate2D {
-    static var userLocation: CLLocationCoordinate2D {
-        return .init(latitude: 30.613, longitude: -96.342)
-    }
-}
+//extension CLLocationCoordinate2D {
+//    static var userCurrentLocation: CLLocationCoordinate2D {
+//        return .init(latitude: 30.619049, longitude: -96.339394)
+//    }
+//}
 
-extension CLLocationCoordinate2D {
-    static var userDestination: CLLocationCoordinate2D {
-        return .init(latitude: 30.619049, longitude: -96.339394)
-    }
-}
 
-extension MKCoordinateRegion {
-    static var userRegion: MKCoordinateRegion {
-        return .init(center: .userLocation, latitudinalMeters: 1000, longitudinalMeters: 1000)
-    }
-}
+//extension MKCoordinateRegion {
+//    static var userRegion: MKCoordinateRegion {
+//        return .init(center: .userLocation, latitudinalMeters: 1000, longitudinalMeters: 1000)
+//    }
+//}
+
+
 
 extension ConfirmPageView {
     
     func fetchRoute() {
         
         let request = MKDirections.Request()
-        request.source = MKMapItem(placemark: .init(coordinate: .userLocation))
-        request.destination = MKMapItem(placemark: .init(coordinate: .userDestination))
+        request.source = MKMapItem(placemark: .init(coordinate: userCurrentLocation))
+        request.destination = MKMapItem(placemark: .init(coordinate: userPickup))
         
         Task {
             let result = try? await MKDirections(request: request).calculate()
             route = result?.routes.first
-            routeDestination =  MKMapItem(placemark: .init(coordinate: .userDestination))
+            routeDestination =  MKMapItem(placemark: .init(coordinate: userPickup))
         }
     }
 }
 
 #Preview {
+<<<<<<< Updated upstream
     ConfirmPageView()
+=======
+    ConfirmPageView(currentTime: "", destinationTime: "", 
+                    userCurrentLocation: CLLocationCoordinate2D(latitude: 0, longitude: 0),
+                    userPickup: CLLocationCoordinate2D(latitude: 0, longitude: 0))
+>>>>>>> Stashed changes
 }
---
+++ Frontend/Frontend/Ride-Selection/RideSelectionView.swift
@@ -0,0 +1,267 @@
+//
+//  RideSelectionView.swift
+//  Frontend
+//
+//  Created by James Stautler on 2/27/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct RideSelectionView: View {
+    @State var navigateToConfirmPage = false
+    @State private var selectedRouteId: Int?
+    @State private var dragOffset = CGSize.zero
+    @State private var routeOptions: [RouteOption] = []
+    
+    @Binding var sourceCoordinates: CLLocationCoordinate2D
+    @Binding var destinationCoordinates: CLLocationCoordinate2D
+    
+    init(_ sourceCoordinates: Binding<CLLocationCoordinate2D>, _ destinationCoordinates: Binding<CLLocationCoordinate2D>) {
+        self._sourceCoordinates = sourceCoordinates
+        self._destinationCoordinates = destinationCoordinates
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading){
+                RouteView($sourceCoordinates, $destinationCoordinates)
+                VStack {
+                    Capsule()
+                        .foregroundColor(Color(.systemGray5))
+                        .frame(width: 48, height: 6)
+                
+                    VStack {
+                        routeSelection
+                        Divider().padding(.vertical, 8)
+                        confirmButton
+                    }
+                }
+                .background(.white)
+            }
+            .onAppear {
+                loadRouteOptions()
+            }
+        }
+    }
+    
+    func loadRouteOptions() {
+        let jsonData = """
+        {
+          "rides": [
+            {
+              "source": {
+                "lat": 30.629979658501668,
+                "long": -96.3324372242465
+              },
+              "pickupPoint": {
+                "lat": 30.6213803,
+                "long": -96.340158
+              },
+              "destination": {
+                "lat": 30.6231958,
+                "long": -96.3285208
+              },
+              "walkTime": 1044,
+              "walkDistance": 0.9003665790000001,
+              "driveTime": 271,
+              "driveDistance": 0.720168989,
+              "totalTime": 1315,
+              "totalDistance": 1.6205355680000002,
+              "price": 10.854228
+            },
+            {
+              "source": {
+                "lat": 30.62178035739474,
+                "long": -96.33854449199474
+              },
+              "pickupPoint": {
+                "lat": 30.6213803,
+                "long": -96.340158
+              },
+              "destination": {
+                "lat": 30.6231958,
+                "long": -96.3285208
+              },
+              "walkTime": 120,
+              "walkDistance": 0.103768957,
+              "driveTime": 296,
+              "driveDistance": 0.893531498,
+              "totalTime": 416,
+              "totalDistance": 0.997300455,
+              "price": 11.031393
+            },
+            {
+              "source": {
+                "lat": 30.62170959960276,
+                "long": -96.34179274951764
+              },
+              "pickupPoint": {
+                "lat": 30.6213803,
+                "long": -96.340158
+              },
+              "destination": {
+                "lat": 30.6231958,
+                "long": -96.3285208
+              },
+              "walkTime": 209,
+              "walkDistance": 0.18019759,
+              "driveTime": 324,
+              "driveDistance": 1.166313367,
+              "totalTime": 533,
+              "totalDistance": 1.346510957,
+              "price": 11.2584305
+            }
+          ]
+        }
+        """.data(using: .utf8)!
+        
+        routeOptions = parseRideOptions(from: jsonData)
+        routeOptions = assignIconsToRouteOptions(routeOptions)
+        if let figureWalkIndex = routeOptions.firstIndex(where: { $0.iconName == "hare.fill" }) {
+                    selectedRouteId = routeOptions[figureWalkIndex].id
+                }
+        self.routeOptions = routeOptions
+    }
+    
+    private var tripInformation: some View {
+        HStack {
+            VStack {
+                Circle()
+                    .fill(Color(.systemGray3))
+                    .frame(width: 8, height: 8)
+                
+                Rectangle()
+                    .fill(Color(.systemGray3))
+                    .frame(width: 1, height: 32)
+                
+                Rectangle()
+                    .fill(.black)
+                    .frame(width: 8, height: 8)
+            }
+            
+            VStack(alignment: .leading, spacing: 24) {
+                HStack {
+                    Text("Current Location")
+                        .font(.system(size: 16, weight: .semibold))
+                        .foregroundColor(.gray)
+                    
+                    Spacer()
+                    
+                    Text(Date().formattedTime())
+                        .font(.system(size: 14, weight: .semibold))
+                        .foregroundColor(.gray)
+                }
+                .padding(.bottom, 10)
+                
+                HStack {
+                    Text("Destination")
+                        .font(.system(size: 16, weight: .semibold))
+                    
+                    Spacer()
+                    
+                    if let selectedRoute = routeOptions.first(where: { $0.id == selectedRouteId }) {
+                        Text("\(selectedRoute.formattedDestinationTime)")
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
+            .padding(.leading, 8)
+        }
+        .padding()
+    }
+    
+    private var routeSelection: some View {
+        VStack {
+            Text("Suggested Routes")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .padding()
+                .foregroundColor(Color.gray)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack(spacing: 12) {
+                ForEach(routeOptions) { option in
+                    Button(action: {
+                        selectedRouteId = option.id
+                    }) {
+                        OptionView(option: option)
+                            .background(selectedRouteId == option.id ? Color.blue.opacity(0.3) : Color(.systemGroupedBackground))
+                            .cornerRadius(10)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                }
+            }
+            .padding(.horizontal)
+        }
+    }
+    
+    private var confirmButton: some View {
+        Button {
+            self.navigateToConfirmPage = true
+        } label: {
+            Text("CONFIRM ROUTE")
+                .fontWeight(.bold)
+                .frame(width: UIScreen.main.bounds.width - 32, height: 50)
+                .background(Color.blue)
+                .cornerRadius(10)
+                .foregroundColor(.white)
+        }
+        .padding(.vertical)
+        .navigationDestination(isPresented: $navigateToConfirmPage) {
+            if let selectedRouteId = selectedRouteId,
+               let selectedRoute = routeOptions.first(where: { $0.id == selectedRouteId }) {
+                ConfirmPageView(currentTime: Date().formattedTime(), destinationTime: selectedRoute.formattedDestinationTime, userCurrentLocation: sourceCoordinates, userPickup: destinationCoordinates)
+            } else {
+                // Fallback or error handling if no route is selected
+                Text("No route selected")
+            }
+        }
+    }
+    
+    struct OptionView: View {
+        let option: RouteOption
+
+        var body: some View {
+            HStack {
+                Image(systemName: option.iconName)
+                    .font(.largeTitle)
+                    .foregroundColor(.blue)
+                    .frame(width: 60)
+
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text(option.formattedWalkTime)
+                            .fontWeight(.bold)
+                        Text("•")
+                        Text(option.formattedWalkDistance)
+                            .fontWeight(.bold)
+                        Spacer()
+                        Text(option.formattedPrice)
+                            .fontWeight(.bold)
+                    }
+                    Text("Arrive by: \(option.formattedDestinationTime)")
+                        .fontWeight(.regular)
+                }
+                Spacer()
+            }
+            .padding()
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+            .cornerRadius(10)
+        }
+    }
+}
+
+extension Date {
+    func formattedTime() -> String {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter.string(from: self)
+    }
+}
+
+
+//#Preview {
+//    RideSelectionView()
+//}
